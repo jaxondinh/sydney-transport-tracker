a70e6dcad2bde6d803c790ca46f46971133905b8
Duy Dinh <jacksonduydinh@gmail.com>
2026-02-26 16:45:39 +1100

    Controller layer completed for alerts

--- src/main/java/com/sydney/transporttracker/controller/AlertController.java
+++ src/main/java/com/sydney/transporttracker/controller/AlertController.java
@@ -1,4 +1,32 @@
 package com.sydney.transporttracker.controller;
 
+import com.sydney.transporttracker.model.Alert;
+import com.sydney.transporttracker.service.AlertService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/alerts")
 public class AlertController {
+    @Autowired
+    private AlertService alertService;
+    @GetMapping
+    public List<Alert> getAlerts() {
+        return alertService.getAllAlerts();
+    }
+    @GetMapping("{id}")
+    public Optional<Alert> getAlertByID(@PathVariable Long id) {
+        return alertService.getAlertByID(id);
+    }
+    @PostMapping
+    public Alert createAlert(@RequestBody Alert alert) {
+        return alertService.createAlert(alert);
+    }
+    @DeleteMapping("{id}")
+    public boolean deleteAlert(@PathVariable Long id) {
+        return alertService.deleteAlert(id);
+    }
 }
