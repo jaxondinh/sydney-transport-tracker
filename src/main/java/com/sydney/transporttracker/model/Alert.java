e737264efe2f074ceaca7e27209531e8f8831cc6
Duy Dinh <jacksonduydinh@gmail.com>
2026-02-26 14:12:25 +1100

    Fixed typing on id, startTime and endTime

--- src/main/java/com/sydney/transporttracker/model/Alert.java
+++ src/main/java/com/sydney/transporttracker/model/Alert.java
@@ -5,17 +5,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 @Entity
 public class Alert {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String lineName;
     private String status;
     private String reason;
-    private LocalTime startTime;
-    private LocalTime endTime;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
     private String affectedStops;
 }
