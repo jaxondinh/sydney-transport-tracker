bac4bc938ed01225457e664aac20fc2aa54d0f2c
Duy Dinh <jacksonduydinh@gmail.com>
2026-02-26 12:45:45 +1100

    Starting project

---
+++ src/main/java/com/sydney/transporttracker/TransporttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sydney.transporttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporttrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransporttrackerApplication.class, args);
+    }
+
+}
