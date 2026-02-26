af21cfa7a9211206b9b0b1c1775befe088b3ed94
Duy Dinh <jacksonduydinh@gmail.com>
2026-02-26 14:26:57 +1100

    Created repo layer for alert and starting work on service layer

---
+++ src/main/java/com/sydney/transporttracker/repository/AlertRepository.java
@@ -0,0 +1,10 @@
+package com.sydney.transporttracker.repository;
+
+import com.sydney.transporttracker.model.Alert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlertRepository extends JpaRepository<Alert, Long> {
+
+}
