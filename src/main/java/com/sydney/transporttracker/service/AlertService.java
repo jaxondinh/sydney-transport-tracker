5f1b7b877d61443c2b4c7f33cedd7c14fff86d09
Duy Dinh <jacksonduydinh@gmail.com>
2026-02-26 16:15:00 +1100

    Creating AlertService first draft

--- src/main/java/com/sydney/transporttracker/service/AlertService.java
+++ src/main/java/com/sydney/transporttracker/service/AlertService.java
@@ -1,4 +1,30 @@
 package com.sydney.transporttracker.service;
 
+import com.sydney.transporttracker.model.Alert;
+import com.sydney.transporttracker.repository.AlertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AlertService {
+    @Autowired
+    private AlertRepository alertRepository;
+    public Alert createAlert(Alert alert) {
+        alertRepository.save(alert);
+        return alert;
+    }
+    public boolean deleteAlert(Alert alert) {
+        alertRepository.deleteById(alert.id);
+        return true;
+    }
+    public List<Alert> getAllAlerts() {
+        List<Alert> alertList = alertRepository.findAll();
+        return alertList;
+    }
+    public Alert getAlertByID(Long alertID) {
+        Alert alert = alertRepository.getAlertById(alertID);
+        return alert;
+    }
 }
