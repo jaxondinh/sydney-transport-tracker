626160247d06d3f60d9f2885948a5db330754ab7
Duy Dinh <jacksonduydinh@gmail.com>
2026-02-27 10:51:33 +1100

    reading api from properties using @value

--- src/main/java/com/sydney/transporttracker/service/GtfsRealtimeService.java
+++ src/main/java/com/sydney/transporttracker/service/GtfsRealtimeService.java
@@ -1,4 +1,10 @@
 package com.sydney.transporttracker.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GtfsRealtimeService {
+    @Value("${tfn.api.key}")
+    private String apiKey;
 }
