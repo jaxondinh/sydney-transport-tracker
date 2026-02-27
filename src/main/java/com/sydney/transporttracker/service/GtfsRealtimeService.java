86f160596e102d72b53fd9d4a070fa9a3f853148
Duy Dinh <jacksonduydinh@gmail.com>
2026-02-27 11:36:16 +1100

    starting parsing of response, added try catch for error handling

--- src/main/java/com/sydney/transporttracker/service/GtfsRealtimeService.java
+++ src/main/java/com/sydney/transporttracker/service/GtfsRealtimeService.java
@@ -1,5 +1,6 @@
 package com.sydney.transporttracker.service;
 
+import com.google.transit.realtime.GtfsRealtime;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,22 @@ public class GtfsRealtimeService {
     private String apiKey;
     private RestTemplate restTemplate = new RestTemplate();
     public void fetchSydneyTrainAlerts() {
-        // Create header
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "apikey " + apiKey);
-        // Tell header what is acceptable, always take list so use singletonList here
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_OCTET_STREAM));
-        // Wrapper needed to be used with restTemplate
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        // Actual API call
-        String url = "https://api.transport.nsw.gov.au/v2/gtfs/alerts/sydneytrains";
-        ResponseEntity<byte[]> response = restTemplate.exchange(url, HttpMethod.GET, entity, byte[].class);
+        try {
+            // Create header
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "apikey " + apiKey);
+            // Tell header what is acceptable, always take list so use singletonList here
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_OCTET_STREAM));
+            // Wrapper needed to be used with restTemplate
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+            // Actual API call
+            String url = "https://api.transport.nsw.gov.au/v2/gtfs/alerts/sydneytrains";
+            ResponseEntity<byte[]> response = restTemplate.exchange(url, HttpMethod.GET, entity, byte[].class);
+            // Parsing
+            byte[] data = response.getBody();
+            GtfsRealtime.FeedMessage feed = GtfsRealtime.FeedMessage.parseFrom(data);
+        } catch (Exception e) {
+            System.out.println("Error fetching GTFS data" + e.getMessage());
+        }
     }
 }
