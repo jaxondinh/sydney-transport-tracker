d45d0cf750280632d45b19b03cd01591c5ba01ea
Duy Dinh <jacksonduydinh@gmail.com>
2026-03-04 11:05:42 +1100

    Finished handleValidationErrors with custom messaging

--- src/main/java/com/sydney/transporttracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sydney/transporttracker/exception/GlobalExceptionHandler.java
@@ -1,4 +1,25 @@
 package com.sydney.transporttracker.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationErrors(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+        return ResponseEntity
+                .badRequest()
+                .body(errors);
+    }
 }
