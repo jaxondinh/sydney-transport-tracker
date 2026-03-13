361eee4702ab6389f431771284992f51330c7a2b
Duy Dinh <jacksonduydinh@gmail.com>
2026-03-13 12:09:52 +1100

    created implementation

--- src/main/java/com/sydney/transporttracker/service/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sydney/transporttracker/service/security/UserDetailsServiceImpl.java
@@ -1,4 +1,30 @@
 package com.sydney.transporttracker.service.security;
 
-public class UserDetailsServiceImpl {
+import com.sydney.transporttracker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.sydney.transporttracker.model.User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username + " not found");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
 }
