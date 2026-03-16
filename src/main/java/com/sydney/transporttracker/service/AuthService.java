27f851119ff06026bbf90bf7eee5e74df820c0a5
Duy Dinh <jacksonduydinh@gmail.com>
2026-03-16 13:17:20 +1100

    corrected dependency imports

--- src/main/java/com/sydney/transporttracker/service/AuthService.java
+++ src/main/java/com/sydney/transporttracker/service/AuthService.java
@@ -1,8 +1,17 @@
 package com.sydney.transporttracker.service;
 
+import com.sydney.transporttracker.service.security.JwtService;
+import com.sydney.transporttracker.service.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
-
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 }
