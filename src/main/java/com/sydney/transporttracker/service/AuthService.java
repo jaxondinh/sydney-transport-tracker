3c45a7f231468667074b281795d359367b704f02
Duy Dinh <jacksonduydinh@gmail.com>
2026-03-16 13:26:28 +1100

    starting work on authcontroller

--- src/main/java/com/sydney/transporttracker/service/AuthService.java
+++ src/main/java/com/sydney/transporttracker/service/AuthService.java
@@ -1,17 +1,22 @@
 package com.sydney.transporttracker.service;
 
+import com.sydney.transporttracker.dto.LoginRequest;
 import com.sydney.transporttracker.service.security.JwtService;
-import com.sydney.transporttracker.service.security.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
     @Autowired
-    private UserDetailsServiceImpl userDetailsService;
-    @Autowired
     private JwtService jwtService;
     @Autowired
     private AuthenticationManager authenticationManager;
+    public String login(LoginRequest loginRequest) {
+        String username = loginRequest.getUsername();
+        String password = loginRequest.getPassword();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        return jwtService.generateToken(username);
+    }
 }
