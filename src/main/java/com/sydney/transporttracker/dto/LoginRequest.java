8ecdbf71a4a9fb719956ede1678dfad9d77f6a79
Duy Dinh <jacksonduydinh@gmail.com>
2026-03-16 13:13:16 +1100

    created dto

--- src/main/java/com/sydney/transporttracker/dto/LoginRequest.java
+++ src/main/java/com/sydney/transporttracker/dto/LoginRequest.java
@@ -1,4 +1,13 @@
 package com.sydney.transporttracker.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
+    private String username;
+    private String password;
 }
