569de4f9f103c43ee465ee3bf16d19354f436cdd
Duy Dinh <jacksonduydinh@gmail.com>
2026-03-16 13:40:51 +1100

    implemented JWT auth - login endpoint, token generation and endpoint protection

--- src/main/java/com/sydney/transporttracker/controller/AuthController.java
+++ src/main/java/com/sydney/transporttracker/controller/AuthController.java
@@ -1,4 +1,21 @@
 package com.sydney.transporttracker.controller;
 
+import com.sydney.transporttracker.dto.LoginRequest;
+import com.sydney.transporttracker.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private AuthService authService;
+    @PostMapping("/login")
+    public String login(@Valid @RequestBody LoginRequest loginRequest) {
+        return authService.login(loginRequest);
+    }
 }
--- src/main/java/com/sydney/transporttracker/model/User.java
+++ src/main/java/com/sydney/transporttracker/model/User.java
@@ -1,14 +1,12 @@
 package com.sydney.transporttracker.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
