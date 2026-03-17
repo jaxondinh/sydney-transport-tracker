bd9ae92ad890d544e99f4d85ffadc4f65e3f4471
Duy Dinh <jacksonduydinh@gmail.com>
2026-03-17 12:53:38 +1100

    added validation to login request

--- src/main/java/com/sydney/transporttracker/dto/LoginRequest.java
+++ src/main/java/com/sydney/transporttracker/dto/LoginRequest.java
@@ -1,5 +1,7 @@
 package com.sydney.transporttracker.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
+    @NotBlank(message = "Please enter a username")
+    @Column(unique = true)
     private String username;
+    @NotBlank(message = "Please enter a password")
     private String password;
 }
