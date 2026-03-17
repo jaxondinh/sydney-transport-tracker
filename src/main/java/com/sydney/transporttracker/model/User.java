36f4660e1b62bb834d78e5d43aba2530f0db289f
Duy Dinh <jacksonduydinh@gmail.com>
2026-03-17 12:46:17 +1100

    added email to user model, created registerrequest dto, added register function to authservice

--- src/main/java/com/sydney/transporttracker/model/User.java
+++ src/main/java/com/sydney/transporttracker/model/User.java
@@ -16,5 +16,6 @@ public class User {
     private Long id;
     private String username;
     private String password;
+    private String email;
     private String role;
 }
--- src/main/java/com/sydney/transporttracker/service/AuthService.java
+++ src/main/java/com/sydney/transporttracker/service/AuthService.java
@@ -1,10 +1,15 @@
 package com.sydney.transporttracker.service;
 
 import com.sydney.transporttracker.dto.LoginRequest;
+import com.sydney.transporttracker.dto.RegisterRequest;
+import com.sydney.transporttracker.model.User;
+import com.sydney.transporttracker.repository.UserRepository;
 import com.sydney.transporttracker.service.security.JwtService;
+import com.sydney.transporttracker.service.security.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +18,22 @@ public class AuthService {
     private JwtService jwtService;
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
     public String login(LoginRequest loginRequest) {
         String username = loginRequest.getUsername();
         String password = loginRequest.getPassword();
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         return jwtService.generateToken(username);
+    }
+    public User register(RegisterRequest registerRequest) {
+        User newUser = new User();
+        newUser.setUsername(registerRequest.getUsername());
+        newUser.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        newUser.setRole("ROLE_USER");
+        newUser.setEmail(registerRequest.getEmail());
+        return userRepository.save(newUser);
     }
 }
