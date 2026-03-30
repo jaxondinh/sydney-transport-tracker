1170464fa5336852305f9d3c4fac8ebfe2ce25c2
Duy Dinh <jacksonduydinh@gmail.com>
2026-03-30 12:02:40 +1100

    created usercontroller layer

---
+++ src/main/java/com/sydney/transporttracker/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.sydney.transporttracker.controller;
+
+import com.sydney.transporttracker.dto.UserProfileResponse;
+import com.sydney.transporttracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    UserService userService;
+    @GetMapping("/me")
+    public UserProfileResponse myProfile(@AuthenticationPrincipal UserDetails userDetails) {
+        return userService.getUserProfile(userDetails.getUsername());
+    }
+}
