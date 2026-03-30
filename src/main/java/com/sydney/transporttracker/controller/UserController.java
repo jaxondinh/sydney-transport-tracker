9c9abaef632b219304cede92439614a8bdc07d24
Duy Dinh <jacksonduydinh@gmail.com>
2026-03-30 12:13:14 +1100

    Added GET /users/me endpoint to get user profile

--- src/main/java/com/sydney/transporttracker/controller/UserController.java
+++ src/main/java/com/sydney/transporttracker/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     UserService userService;
+
     @GetMapping("/me")
     public UserProfileResponse myProfile(@AuthenticationPrincipal UserDetails userDetails) {
         return userService.getUserProfile(userDetails.getUsername());
