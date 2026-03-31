430b6067b5d42b80d369a92307d7961929f1bb62
Duy Dinh <jacksonduydinh@gmail.com>
2026-03-31 12:19:40 +1100

    starting work on sending alerts to ai for summary

---
+++ src/main/java/com/sydney/transporttracker/dto/SummaryRequest.java
@@ -0,0 +1,12 @@
+package com.sydney.transporttracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SummaryRequest {
+    private String
+}
